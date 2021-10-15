09c70c7ddc2cc0604f75558ec3d163350c33447f
carled7 <carlos_ed03@outlook.com>
2021-10-15 19:04:32 -0300

    finished exercise_01 of 28_set

--- 28_set/exercise_01.c
+++ 28_set/exercise_01.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printVariance(float variance){
+    system("clear");
+    printf("Variance: %f\n\n\n\n", variance);
+}
+
+float calcVariance(int* values, float average, int n){
+
+    float aux = 0;
+    for(int i = 0; i < n; i++){
+        aux += (values[i] - average) * (values[i] - average);
+    }
+    float variance = aux/average;
+    return variance;
+}
+
+float calcAverage(int* values, int* n){
+
+    int sum = 0;
+    for(int i = 0; i < *n; i++){
+        sum += values[i];
+    }
+    float average = sum/ *n;
+    return average;
+}
+
+int* inputValues(int *n){
+
+    printf("How many number to input? ");
+    scanf("%d", &*n);
+
+    int* values = malloc(sizeof(int) * *n);
+
+    for(int i = 0; i < *n; i++){
+        printf("\n- Number n%d: ", i + 1);
+        scanf("%d", &values[i]);
+    }
+
+    return values;
+}
+
 int main (){
 
+    int n;
+    int* values = inputValues(&n);
+    //for(int i = 0; i < 2; i++){
+    //    printf("\n- N n%d: %d", i + 1, values[i]);
+    //}
+    float average = calcAverage(values, &n);
+    float variance = calcVariance(values, average, n);
+
+    printVariance(variance);
+    
     return 0;
 }
